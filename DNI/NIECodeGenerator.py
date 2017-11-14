914062d1dbf6dfd1b05806c9f61a5da24dc9e6f6
Arg0s <ivan.rincon76@gmail.com>
2017-11-14 21:32:15 +0100

    Actualización archivos

--- DNI/NIECodeGenerator.py
+++ DNI/NIECodeGenerator.py
@@ -1,4 +1,22 @@
-from DNI.DNIeCodeGenerator import *
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+#
+# DNI_Code_Generator is licensed under the:
+# GNU General Public License v3.0
+#
+# Permissions of this strong copyleft license are conditioned
+# on making available complete source code of licensed works
+# and modifications, which include larger works using a licensed
+# work, under the same license. Copyright and license notices
+# must be preserved. Contributors provide an express grant of
+# patent rights.
+#
+# For more information on this, and how to apply and follow the
+# GNU GPL, see http://www.gnu.org/licenses
+#
+# Iván Rincón 2017
+
+from DNI.DNICodeGenerator import DNICodeGenerator
 from DNI.NIEHashGenerator import NIEHashGenerator
 
 
@@ -32,7 +50,3 @@ class NIECodeGenerator(DNICodeGenerator, NIEHashGenerator):
                 self.serial_number().ljust(10, "<") +
                 self.serial_number_hash() +
                 self.full_dni()).ljust(30, "<")
-
-
-xxx = NIECodeGenerator("E07275385", "X7665889", "800220", "110509", "M", "AUT")
-print(xxx)
--- DNI/NIEHashGenerator.py
+++ DNI/NIEHashGenerator.py
@@ -1,3 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+#
+# DNI_Code_Generator is licensed under the:
+# GNU General Public License v3.0
+#
+# Permissions of this strong copyleft license are conditioned
+# on making available complete source code of licensed works
+# and modifications, which include larger works using a licensed
+# work, under the same license. Copyright and license notices
+# must be preserved. Contributors provide an express grant of
+# patent rights.
+#
+# For more information on this, and how to apply and follow the
+# GNU GPL, see http://www.gnu.org/licenses
+#
+# Iván Rincón 2017
+
 from DNI.DNIeHashGenerator import DNIeHashGenerator
 
 
